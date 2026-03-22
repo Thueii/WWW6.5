415a0d86c11cb3c4fdabe56ebebe7944fd121727
mara <798044004@qq.com>
2026-03-22 18:25:03 +1100

    fix: change dependency

--- mara/Day18_MockEthUsdPriceFeed.sol
+++ mara/Day18_MockEthUsdPriceFeed.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract MockEthUsdPriceFeed is AggregatorV3Interface {
     int256 private _price;
